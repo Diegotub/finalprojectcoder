83e50c3e12af53a2e50bc2f6e09195203ac01366
Diego Tellechea <diegotellechea@192.168.1.29>
2022-10-31 15:44:45 -0300

    primer commit de mi proyecto

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Diego",
+    "apellido":"Tellechea"})
