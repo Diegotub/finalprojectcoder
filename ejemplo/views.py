1c60cd8883cc91834717ecb057ffd6cb339d2c29
Diego Tellechea <diegotellechea@192.168.1.29>
2022-10-31 16:11:38 -0300

    primer commit de mi proyecto

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, "ejemplo/saludar.html", {"nombre":"Diego",
     "apellido":"Tellechea"})
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", {"nombre":"Diego",
+    "apellido":"Tellechea"})
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html", {"notas": [1,2,3,4,5,6,7,8]})
