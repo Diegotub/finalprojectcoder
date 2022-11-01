cb196fe6758dbd8345ad0e98df78e4d7654070cc
Diego Tellechea <diegotellechea@192.168.1.29>
2022-10-31 23:41:23 -0300

    + commit

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path 
-from ejemplo.views import index, index_dos, index_tres, monstrar_familiares
+from ejemplo.views import index, index_dos, index_tres, monstrar_familiares, BuscarFamiliar
 from blog.views import index as blog_index
 
 urlpatterns = [
@@ -25,4 +25,6 @@ urlpatterns = [
     path('mostrar-notas/', index_tres),
     path('mi-familia/', monstrar_familiares),
     path('blog/', blog_index),
-]
+    path('mi-familia/buscar', BuscarFamiliar.as_view()), # NUEVA RUTA PARA BUSCAR FAMILIAR
+  ]
+
