17effea4cfe8a65d89726bff9c1c57d1697fe9e2
Diego Tellechea <diegotellechea@192.168.1.29>
2022-11-03 10:08:10 -0300

    models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Configuarcion(models.Model):
+    nombre_blog = models.CharField(max_lenght=14)
+    contruido_por = models.CharField(max_lenght=30)
+    titulo_principal = models.CharField(max_lenght=40)
+    subtitulo_principal = models.CharField(max_lenght=40)
+
+class Post(models.Model):
+    title = models.CharField(max_lenght=100)
+    short_content = models.CharField(max_lenght=255)
+    content = models.CharField(max_lenght=3000)
+    date_published = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(upload_to= "posts", null=True, blank=True)
+
+    def __str__(self):
+        return f"id:{self.id}, title:{self.title}"
