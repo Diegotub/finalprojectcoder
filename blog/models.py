8ed069d0a5041e28f4638c8578bc3d9428d38579
Diego Tellechea <diegotellechea@192.168.1.29>
2022-11-03 12:16:02 -0300

    commit migrations

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-class Configuarcion(models.Model):
-    nombre_blog = models.CharField(max_lenght=14)
-    contruido_por = models.CharField(max_lenght=30)
-    titulo_principal = models.CharField(max_lenght=40)
-    subtitulo_principal = models.CharField(max_lenght=40)
+class Configuracion(models.Model):
+    nombre_blog = models.CharField(max_length=14)
+    contruido_por = models.CharField(max_length=30)
+    titulo_principal = models.CharField(max_length=40)
+    subtitulo_principal = models.CharField(max_length=40)
 
 class Post(models.Model):
-    title = models.CharField(max_lenght=100)
-    short_content = models.CharField(max_lenght=255)
-    content = models.CharField(max_lenght=3000)
+    title = models.CharField(max_length=100)
+    short_content = models.CharField(max_length=255)
+    content = models.CharField(max_length=3000)
     date_published = models.DateTimeField(auto_now_add = True)
     image = models.ImageField(upload_to= "posts", null=True, blank=True)
 
     def __str__(self):
         return f"id:{self.id}, title:{self.title}"
+
